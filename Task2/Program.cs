64d03bd1369b6b3044c6903335c0fc24f1c40078
Nata <nata.9688@mail.ru>
2022-09-06 14:21:52 +0400

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// вывод 3ей цифры заданного числа или сообщает, что третьей цифры нет
+
+void ThirdDigit(int number)
+{
+   if (number>99 & 999>number) Console.WriteLine($"Третья цифра {number%10}");
+   else if  (number>999 & 9999>number) Console.WriteLine($"Третья цифра {number%100/10}");
+   else if  (number>9999 & 99999>number) Console.WriteLine($"Третья цифра {number%1000/100}");
+   else Console.WriteLine("Третьей цифры нет");
+}
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+ThirdDigit(number);
