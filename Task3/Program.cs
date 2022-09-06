d8c0dc28c24384ce17e6d1d91b11bac4b43f6875
Nata <nata.9688@mail.ru>
2022-09-06 16:28:20 +0400

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// На вход принимает цифру, обозначающую день недели и проверяет, является ли это выходным днем
+
+void Weekend ()
+{
+    int number = new Random().Next(1,8);
+    Console.WriteLine($"Случайно сгенерированное число {number}");
+    if (number == 6) Console.WriteLine("Да, сегодня суббота!");
+    else if (number ==7) Console.WriteLine("Да, сегодня воскресенье!");
+    else Console.WriteLine("Будний день");
+}
+
+Weekend();
+
